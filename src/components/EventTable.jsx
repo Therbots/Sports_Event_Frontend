6ccbdf6067fd1046382c8a317d2ee8ef19b68cdb
taylor herbots <therbots@live.com>
2021-10-12 18:45:46 -0500

    created eventtable.jsx

---
+++ src/components/EventTable.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class EventTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <table>
+                
+            </table>
+         );
+    }
+}
+ 
+export default EventTable;
